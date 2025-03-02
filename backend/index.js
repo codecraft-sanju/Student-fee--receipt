cfd618c0ddbf11a03dc05bf152b33713051262ea
codecraft-sanju <sanjusirvich18@gmail.com>
2025-03-02 15:00:49 +0530

    index improve

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.post('/send-email', async (req, res) => {
       pass: process.env.EMAIL_PASS, 
     },
     tls: {
-      rejectUnauthorized: false, // SSL Error fix
+      rejectUnauthorized: false, // SSL Error 
     },
   });
 
